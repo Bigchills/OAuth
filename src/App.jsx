6cd1e1103ce0a1aa35d7567fce62b1e910ced8d5
Bigchills <yemi7164@gmail.com>
2024-05-04 15:38:25 +0100

    updated_auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/hero'
 import Auth from './components/Auth'
+import SignIn from './Pages/SignIn'
+import Homepage from './Pages/Homepage'
+import { BrowserRouter, Routes, Route, Link, Navigate } from 'react-router-dom'
+
 
 const App = () => {
+  const user=false;
   return (
-    <>
-      <Navbar/>
-      <Hero/>
-      <Auth/>
-
-
-    </>
+    <BrowserRouter>
+      <>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Homepage/>}/>
+          <Route path='/signin' element={<SignIn/>}/>
+        </Routes>       
+      </>
+    </BrowserRouter>
   )
 }
-
 export default App
---
+++ src/Pages/SignIn.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/hero'
+import Auth from '../components/Auth'
+
+const SignIn = () => {
+  return (
+    <div>
+        <Hero/>
+        <Auth/>    
+    </div>
+  )
+}
+
+export default SignIn
