019bc1106c65b5ff3336ed9b0fda2835dd2d67b4
Bigchills <yemi7164@gmail.com>
2024-06-09 23:21:08 +0100

    token check

--- src/components/Mainnav.jsx
+++ src/components/Mainnav.jsx
@@ -5,42 +5,11 @@ import axios from 'axios'
 
 const Mainnav = () => {
 
-  const [userData, setUserData] = useState(null);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    // Retrieve the authentication token from localStorage
-    const authToken = localStorage.getItem('authToken');
-    // Define the URL of your backend endpoint to fetch user data
-    const apiUrl = 'https://roomie-app-1.onrender.com/auth/user';
-
-    // Make a fetch request to the backend API
-    fetch(apiUrl, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${authToken}` // Include the token in the Authorization header
-      }
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json(); // Parse the JSON response
-    })
-    .then(data => {
-      setUserData(data); // Set the user data state
-    })
-    .catch(error => {
-      setError(error.message); // Set the error state
-    });
-  }, []); // Empty dependency array to ensure useEffect runs only once
-
-
-
+  const authToken=localStorage.getItem('authToken');
+  console.log(authToken);
 
   return (
     <div>
-      {userData &&(
         <nav className="bg-blue-700 flex justify-between items-center
                          px-6 p-3 border-b-2 border-white                                             
                          ">
@@ -52,7 +21,7 @@ const Mainnav = () => {
               <img src="/images/react.svg" alt="userPhoto" className=" rounded-full h-7 w-7"/>
             </div>
             <div>
-              <h5>{userData.username}</h5>
+              <h5>username</h5>
             </div>
             <div>
               <button>
@@ -60,8 +29,7 @@ const Mainnav = () => {
               </button>
             </div>
           </div>
-        </nav>
-      )}      
+        </nav>     
     </div>
   )
 }
