b06fea30859e90d3b852bbf5a34c54c11bed86bb
Bigchills <yemi7164@gmail.com>
2024-06-16 02:05:17 +0100

    signup fix

--- src/components/Newaccountmodal.jsx
+++ src/components/Newaccountmodal.jsx
@@ -46,8 +46,8 @@ const Newaccountmodal = ({ closeModal }) => {
   };
 
   return (
-    <div className="w-full h-full fixed top-0 backdrop-blur-sm md:py-5  flex justify-center items-center">
-      <div className="w-full h-full  md:max-w-2xl md:max-h-[550px] z-10 bg-white font rounded-lg border  border-slate-400 mx-auto overflow-y-scroll overscroll-y-none">
+    <div className="w-full h-full fixed top-0 backdrop-blur-sm md:py-5 flex justify-center items-center">
+      <div className="w-full h-full md:max-w-2xl md:max-h-[550px] z-10 bg-white font rounded-lg border border-slate-400 mx-auto overflow-y-scroll overscroll-y-none">
         <div className="font-medium text-center">logo</div>
         <div className="font-medium flex justify-center items-center">
           <h2>Fill in your details</h2>
@@ -56,23 +56,65 @@ const Newaccountmodal = ({ closeModal }) => {
         <form onSubmit={handleSubmit}>
           <div className="flex flex-col w-3/4 md:max-w-sm mt-10 mx-auto space-y-1">
             <label className="font-medium" htmlFor="name">Full Name:</label>
-            <input id="name" value={fullName} onChange={(e) => setFullName(e.target.value)} className="bg-slate-100 border-slate-600  rounded-lg p-3" placeholder="Your name" type="text" required />
+            <input
+              id="name"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              className="bg-slate-100 border-slate-600 rounded-lg p-3"
+              placeholder="Your name"
+              type="text"
+              required
+            />
           </div>
           <div className="flex flex-col w-3/4 md:max-w-sm mx-auto space-y-1 mt-5 mb-10">
             <label className="font-medium" htmlFor="phonenumber">Phone number:</label>
-            <input id="phonenumber" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} className="bg-slate-100 border-slate-600  rounded-lg p-3" placeholder="Your phone number" type="text" required />
+            <input
+              id="phonenumber"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              className="bg-slate-100 border-slate-600 rounded-lg p-3"
+              placeholder="Your phone number"
+              type="text"
+              required
+            />
           </div>
           <div className="flex flex-col w-3/4 md:max-w-sm mx-auto space-y-1 mt-5 mb-10">
             <label className="font-medium" htmlFor="email">Email:</label>
-            <input id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="bg-slate-100 border-slate-600 rounded-lg p-3" placeholder="Your email" type="email" required />
+            <input
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="bg-slate-100 border-slate-600 rounded-lg p-3"
+              placeholder="Your email"
+              type="email"
+              required
+            />
           </div>
           <div className="flex flex-col w-3/4 md:max-w-sm mx-auto space-y-1 mt-5 mb-10">
             <label className="font-medium" htmlFor="password">Password:</label>
-            <input id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="bg-slate-100 border-slate-600  rounded-lg p-3" placeholder="Type a password" type="password" required />
+            <input
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="bg-slate-100 border-slate-600 rounded-lg p-3"
+              placeholder="Type a password"
+              type="password"
+              required
+            />
           </div>
           <div className="w-1/2 flex mx-auto space-x-5 justify-center items-center mb-5">
-            <button onClick={() => closeModal(false)} className="border bg-red-600 hover:bg-red-700  rounded-md text-white px-3 py-1 text-sm">Cancel</button>
-            <button type="submit" className="border bg-black hover:bg-slate-800 rounded-md text-white px-3 py-1 text-sm">Sign up</button>
+            <button
+              onClick={() => closeModal(false)}
+              className="border bg-red-600 hover:bg-red-700 rounded-md text-white px-3 py-1 text-sm"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="border bg-black hover:bg-slate-800 rounded-md text-white px-3 py-1 text-sm"
+            >
+              Sign up
+            </button>
           </div>
         </form>
       </div>
