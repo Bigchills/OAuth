680f17b7a244c5d59a8f79426c81ced40bef3502
Bigchills <yemi7164@gmail.com>
2024-06-17 04:12:32 +0100

    data fix

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -16,12 +16,48 @@ const Homepage = () => {
     }
   }, [location, navigate]);
 
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUserId = localStorage.getItem('userId');
+
+    if (!storedUserId) {
+      // Redirect to login or signup page if userId is not found
+      navigate('/');
+      return;
+    }
+
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch(`https://roomie-app-1.onrender.com/auth/user/${storedUserId}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            // Add any necessary authorization headers if required
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch user data');
+        }
+
+        const userData = await response.json();
+        console.log(userData)
+        setUser(userData);
+      } catch (error) {
+        console.error('Error fetching user data:', error.message);
+        // Handle error (e.g., show error message to user)
+      }
+    };
+
+    fetchUserData();
+  }, [navigate]);
 
   
   
   return (
     <div>
-      <Mainnav/>
+      <Mainnav user={user}/>
       <div>
         <h1 className="text-4xl font-bold text-center
                       mt-48"> Welcome 
