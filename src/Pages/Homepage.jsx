b60460e7cba4ed3cbc88e268d841e56e9cae970e
Bigchills <yemi7164@gmail.com>
2024-07-24 19:11:50 +0100

    navigation fix

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -12,15 +12,12 @@ const Homepage = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Extract token from URL
     const urlParams = new URLSearchParams(location.search);
     const token = urlParams.get('auth');
 
     if (token) {
-      // Store token in local storage
       localStorage.setItem('token', token);
 
-      // Fetch authenticated user using the token
       fetch('https://roomie-app-1.onrender.com/auth/user', {
         method: 'GET',
         headers: {
@@ -36,9 +33,7 @@ const Homepage = () => {
       .then(data => {
         setUser(data.data);
         localStorage.setItem('user', JSON.stringify(data.data));
-        // Redirect to the home page
         navigate('/home', { replace: true });
-        console.log(data);
       })
       .catch(error => {
         setError(error);
@@ -47,6 +42,8 @@ const Homepage = () => {
       const storedUser = localStorage.getItem('user');
       if (storedUser) {
         setUser(JSON.parse(storedUser));
+      } else {
+        navigate('/signin', { replace: true });
       }
     }
   }, [location.search, navigate]);
@@ -54,18 +51,20 @@ const Homepage = () => {
   if (error) {
     return <div>Error: {error.message}</div>;
   }
- 
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className=''>
-      <div className='flex '>
-        <Leftsidebar/>
-        <div className='flex-1 flex flex-col'>
+      <div className='flex h-screen'>
+        <Leftsidebar />
+        <div className='flex-1 flex flex-col overflow-y-auto'>
           <Mainnav user={user} />
-          <Maincontent user={user}/>
+          <Maincontent user={user} />
         </div>
-        <RightSidebar/>
-
+        <RightSidebar />
       </div>
     </div>
   );
