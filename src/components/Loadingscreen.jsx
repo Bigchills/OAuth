4f633e4ab3cc36e86554782e16bd93d65aff4f58
Bigchills <yemi7164@gmail.com>
2024-08-04 00:40:15 +0100

    loading fix 2

--- src/components/Loadingscreen.jsx
+++ src/components/Loadingscreen.jsx
@@ -5,7 +5,7 @@ const Loadingscreen = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-75 z-50">
 
-        <div className='bg-white max-h-80 max-w-80'>
+        <div className='bg-white h-96 w-96'>
             <ClipLoader
                 color="#4B70F5"
                 speedMultiplier={0}
