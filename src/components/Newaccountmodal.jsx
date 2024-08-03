ca83c3844f3132fac592e8034b65bacbaed806aa
Bigchills <yemi7164@gmail.com>
2024-08-04 00:48:55 +0100

    l

--- src/components/Newaccountmodal.jsx
+++ src/components/Newaccountmodal.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Loadingscreen from './Loadingscreen';
 
 
 const Newaccountmodal = ({ closeModal }) => {
